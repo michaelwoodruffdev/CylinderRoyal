98686c30427f90f684adc366b0dc9ea46d20d654
Michael Woodruff <michaelwoodruffdev@gmail.com>
2019-02-24 10:01:21 -0800

    add grapple mechanci
    add test environment
    add grapple mechanic
    add monocle (that's the biggest one)

---
+++ Assets/Scripts/gunController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gunController : MonoBehaviour
+{
+    // Components
+    private Transform aimspotTransform;
+    private Transform gunTransform;
+
+    private bool isShot;
+    private Vector3 latchPosition;
+    private float shotRange;
+    private RaycastHit hit;
+
+    private playerController playerControllerRef;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        aimspotTransform = GameObject.Find("aimspot").transform;
+        gunTransform = GetComponent<Transform>();
+
+        isShot = false;
+        latchPosition = new Vector3(0, 0, 0);
+        shotRange = 40f;
+        hit = new RaycastHit();
+
+        playerControllerRef = GameObject.Find("player").GetComponent<playerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("gun position: " + gunTransform.position);
+        Debug.Log("latch position: " + latchPosition);
+        if (isShot)
+        {
+            Debug.DrawRay(gunTransform.position, latchPosition - gunTransform.position, Color.green);
+            Debug.DrawLine(gunTransform.position, latchPosition, Color.green);
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                Vector3 launchForce = (latchPosition - gunTransform.position);
+                launchForce.y += (Vector3.Distance(gunTransform.position, latchPosition));
+                launchForce = Vector3.Normalize(launchForce) * 2000f;
+                playerControllerRef.launch(launchForce);
+                isShot = false;
+            }
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                isShot = false;
+            }
+        }
+
+        Debug.DrawRay(gunTransform.position, aimspotTransform.position - gunTransform.position, Color.cyan);
+        if (Input.GetKeyDown(KeyCode.E) && Physics.Raycast(gunTransform.position, aimspotTransform.position - gunTransform.position, out hit, shotRange))
+        {
+            Ray ray = new Ray(gunTransform.position, aimspotTransform.position - gunTransform.position);
+            latchPosition = ray.GetPoint(hit.distance);
+            isShot = true;
+        }
+
+    }
+}
---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,166 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+    // components
+    private Transform playerTransform;
+    private Rigidbody playerRigidBody;
+
+    // rotation fields
+    private float rotateSpeed;
+
+    // translation fields
+    private float moveSpeed;
+    private Vector3 translation;
+
+    // jump fields
+    private bool jumped;
+    private Vector3 jumpForceVector;
+    private float jumpForce;
+    private bool isMidAir;
+
+    // scale vector fields for crouching and standing
+    private Vector3 standVector;
+    private Vector3 crouchVector;
+
+    private Vector3 launchForce;
+    private bool isLaunched;
+
+	// INITIALIZE
+    /////////////
+	void Start () {
+        initializeFields();
+    }
+
+    void initializeFields()
+    {
+        // components
+        playerTransform = GetComponent<Transform>();
+        playerRigidBody = GetComponent<Rigidbody>();
+
+        // rotation fields
+        rotateSpeed = 200f;
+
+        // translation fields
+        moveSpeed = 10f;
+
+        // jump fields
+        jumped = false;
+        jumpForce = 500f;
+        jumpForceVector = new Vector3(0, jumpForce, 0);
+        isMidAir = false;
+
+        // scale vector fields for crouching and standing
+        standVector = new Vector3(0.5778568f, 0.7373421f, 0.5068351f);
+        crouchVector = new Vector3(0.5778568f, 0.4373421f, 0.5068351f);
+
+        isLaunched = false;
+        launchForce = new Vector3(0, 0, 0);
+    }
+
+    // UPDATE
+    /////////
+    void Update () {
+        handleMovement();
+	}
+
+    void handleMovement()
+    {
+            playerTransform.Rotate(0, Input.GetAxis("Mouse X") * Time.deltaTime * rotateSpeed, 0);
+
+        if (!sprint() && !crouch())
+        {
+            walk();
+        }
+
+        Vector3 translation = calculateTranslation();
+        playerTransform.Translate(translation * Time.deltaTime);
+
+        if (!isMidAir && Input.GetKeyDown(KeyCode.Space))
+            jumped = true;
+    }
+
+    Vector3 calculateTranslation()
+    {
+        float deltaZ = 0;
+        float deltaX = 0;
+
+        if (Input.GetKey(KeyCode.A)) deltaZ -= moveSpeed;
+        if (Input.GetKey(KeyCode.D)) deltaZ += moveSpeed;
+        if (Input.GetKey(KeyCode.W)) deltaX -= moveSpeed;
+        if (Input.GetKey(KeyCode.S)) deltaX += moveSpeed;
+
+        return new Vector3(deltaX, 0, deltaZ);
+    }
+
+    bool sprint ()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            playerTransform.localScale = standVector;
+            moveSpeed = 15f;
+            return true;
+        }
+        return false;
+    }
+
+    bool crouch ()
+    {
+        if (Input.GetKey(KeyCode.C))
+        {
+            playerTransform.localScale = crouchVector;
+            moveSpeed = 4f;
+            return true;
+        }
+        return false;
+    }
+
+    void walk ()
+    {
+        playerTransform.localScale = standVector;
+        moveSpeed = 10f;
+    }
+
+    // public method for grapple to launch player
+    public void launch(Vector3 launchForce)
+    {
+        this.launchForce = launchForce;
+        isLaunched = true;
+    }
+
+    // UNITY PHYSICS FUNCTIONS
+    //////////////////////////
+    void FixedUpdate ()
+    {
+        if (jumped)
+        {
+            Debug.Log("WHATTUP");
+            playerRigidBody.AddForce(jumpForceVector);
+            jumped = false;
+        }
+
+        if (isLaunched)
+        {
+            playerRigidBody.AddForce(launchForce);
+            isLaunched = false;
+        }
+    }
+
+    void OnCollisionEnter (Collision col)
+    {
+        if (col.gameObject.name == "ground")
+        {
+            isMidAir = false;
+        }
+    }
+
+    void OnCollisionExit (Collision col)
+    {
+        if (col.gameObject.name == "ground")
+        {
+            isMidAir = true;
+        }
+    }
+}
